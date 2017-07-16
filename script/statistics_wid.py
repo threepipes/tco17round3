6905c235e2841f66615c9fbd10cee1a1e93bfeec
threepipes_s <threepipesatos@gmail.com>
2017-07-16 17:06:24 +0900

    縦軸に幅，横軸に毒をとったときの変化をplot

--- script/statistics_wid.py
+++ script/statistics_wid.py
@@ -32,15 +32,130 @@ def load_ondemand(path, poison=None, wine=None, strip=None, test_round=None):
     return result
 
 
+def check_border(strip=1, wine=2000):
+    """
+    widが最後に90になるときのpoisonを求める
+    """
+
+    print('stat strip:%d wine:%d' % (strip, wine))
+    csv_data_list = load_ondemand(
+        '../data/bestwid_poison.csv',
+        strip=strip, wine=wine)
+    data = [[] for _ in range(10)]
+    for row in csv_data_list:
+        data[row['round'] - 1].append(row)
+
+    border = []
+    for i in range(10):
+        x_last = -1
+        y_pre = -1
+        y_nx = -1
+        for j, row in enumerate(data[i][:-1]):
+            x = row['poison']
+            y = row['wid']
+            y_next = data[i][j + 1]['wid']
+            if y >= 90 and y_next <= 90:
+                x_last = x
+                y_pre = y
+                y_nx = y_next
+        per = - (90 - y_pre) / (y_pre - y_nx)
+        print('round:%2d x:%f' % (i + 1, x_last + per))
+        border.append(x_last + per)
+    return border
+
+
+def border_diffs():
+    border_list = []
+    for i in range(20):
+        border_list.append(check_border(strip=(i + 1)))
+
+    x = np.arange(1, 11, 1)
+    borders = [[] for _ in range(10)]
+    for i in range(len(border_list) - 1):
+        s = "%2d -> %2d: " % (i, i + 1)
+        for j, (p1, p2) in enumerate(zip(border_list[i], border_list[i + 1])):
+            s += "%2.6f " % (p2 - p1)
+            borders[j].append(p2 - p1)
+        print(s)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('round')
+    ax.set_ylabel('strip(wid=90)')
+    plt.xticks(np.arange(1, 20, 1))
+    plt.grid()
+    plt.yticks(np.arange(0, 20, 1))
+
+    for i, border_y in enumerate(border_list):
+        y = np.array(border_y)
+        ax.plot(x, y, color=(0, i / 20, 0))
+
+    freq = 3
+    for i in range(freq):
+        y_adj = 21 * 2 / (x + 1)# + (0.46 / (i + 1)) * np.log10(x)
+        ax.plot(x, y_adj, color=((i + 1) / (freq + 1), 0, 0))
+
+    plt.savefig('../data/border_01_adj.png')
+    plt.close()
+
+    print('all:')
+    diff = []
+    s = ''
+    for p1, p2 in zip(border_list[0], border_list[-1]):
+        diff.append((p2 - p1) / 19)
+        s += "%2.6f " % ((p2 - p1) / 19)
+    print(s)
+    for i in range(len(diff) - 1):
+        print(diff[i + 1] - diff[i])
+    # plt.ylim((0, 10))
+    # plt.xlim((0, 10))
+    # plt.xticks(np.arange(1, 11, 1))
+    # plt.yticks(np.arange(0, 0.5, 0.05))
+
+    # ax.plot(x, diff)
+    # for i in range(3):
+    # ax.plot(x, 0.5 - 1 / (x + 1), color=(1, 0, 0))
+    # ax.plot(x, np.log10(x) * 0.46, color=(0, 1, 0))
+    # ax.plot(x, np.log2(x) * 0.14, color=(0, 0, 0))
+    # plt.savefig('../data/border_02_adj.png')
+    # plt.close()
+
+
+
+def stat_poison_wid_rel(y_align, strip=8, wine=2000):
+    """
+    strip, wineを固定したとき，
+    roundごとに(strip, wid)を頂点とした折れ線グラフを描く
+    """
+
+    print('stat strip:%d wine:%d' % (strip, wine))
+    csv_data_list = load_ondemand(
+        '../data/bestwid_poison.csv',
+        strip=strip, wine=wine)
+    data = [[] for _ in range(10)]
+    for row in csv_data_list:
+        data[row['round'] - 1].append(row)
+
+    for i in range(10):
+        xy = []
+        for row in data[i]:
+            x = row['poison']
+            y = row[y_align]
+            print('round:%2d p:%d w:%d wid*poison=%d' % (i + 1, x, y, x * y))
+            if x >= 10:
+                xy.append(x * y)
+        print('med: ', np.median(xy))
+
+
 def plot_poison_wid_rel(y_align, strip=8, wine=2000, ylim=None, path_dir='../data/plot/'):
     """
-    poison, wineを固定したとき，
+    strip, wineを固定したとき，
     roundごとに(strip, wid)を頂点とした折れ線グラフを描く
     """
 
     print('plot strip:%d wine:%d' % (strip, wine))
     csv_data_list = load_ondemand(
-        '../data/bestwid_poison_%2d.csv' % strip,
+        '../data/bestwid_poison.csv',
         strip=strip, wine=wine)
     data = [[] for _ in range(10)]
     for row in csv_data_list:
@@ -50,10 +165,10 @@ def plot_poison_wid_rel(y_align, strip=8, wine=2000, ylim=None, path_dir='../dat
     ax = fig.add_subplot(111)
     ax.set_xlabel('poison')
     ax.set_ylabel(y_align)
-    plt.xticks(np.arange(1, 21, 1))
+    plt.xticks(np.arange(0, 101, 5))
     plt.grid()
     if ylim:
-        plt.yticks(np.arange(ylim[0], ylim[1], (ylim[1] - ylim[0]) / 10))
+        plt.yticks(np.arange(ylim[0], ylim[1], (ylim[1] - ylim[0]) / 20))
         plt.ylim(ylim)
 
     for i in range(10):
@@ -65,16 +180,23 @@ def plot_poison_wid_rel(y_align, strip=8, wine=2000, ylim=None, path_dir='../dat
             y.append(row[y_align])
         ax.plot(x, y, color=color)
 
+    for i in range(10):
+        r = i + 1
+        color = ((10 - i) * 0.1, 0, 0)
+        x = np.arange(1, 101, 1)
+        y = 2 * wine / ((x - 0.46 * np.log10(r) * (strip - 3)) * (r*1.1 + 1))
+        ax.plot(x, y, color=color)
+
     plt.savefig(path_dir + 'poison_widfig_%s_w%04d_s%02d.png' % (y_align, wine, strip))
     plt.close()
 
 
 def plot_poison_wid():
-    path_dir = '../data/plot_poison_wid/'
+    path_dir = '../data/plot_poison_wid_wna/'
     if not os.path.exists(path_dir):
         os.makedirs(path_dir)
     for strip in range(1, 21, 1):
-        plot_poison_wid_rel('prob', strip=strip, ylim=(0, 1), path_dir=path_dir)
+        plot_poison_wid_rel('wid', strip=strip, ylim=(0, 100), path_dir=path_dir)
 
 
 def plot_strip_wid_rel(y_align, poison=8, wine=681, ylim=None, path_dir='../data/plot/'):
